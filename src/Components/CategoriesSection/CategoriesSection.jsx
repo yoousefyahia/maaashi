fb91d04990e16b0d9b87a469eefd21a6fee8a7f4
yousef yahia <yoousefyahia@gmail.com>
2025-11-18 14:19:12 +0200

    fix token type and add genral auth seeting &fix login success

--- src/Components/CategoriesSection/CategoriesSection.jsx
+++ src/Components/CategoriesSection/CategoriesSection.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import "./CategoriesSection.css";
+import "./categoriesSection.css";
 
 const CategoriesSection = () => {
   const [categories, setCategories] = useState([]);
