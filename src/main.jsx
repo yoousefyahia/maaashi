7974f0de04aa8a1c66fd918224f1db305951bffc
yousef yahia <yoousefyahia@gmail.com>
2025-11-18 14:09:25 +0200

    fix token type and add genral auth seeting &fix login success

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./Styles/global.css";
-import "./styles/variables.css";
+import "./Styles/variables.css";
 import { CookiesProvider } from "react-cookie";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
