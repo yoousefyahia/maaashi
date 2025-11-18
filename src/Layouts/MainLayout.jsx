c46eae5a8aabe4bef217562733b0e193b9cba22c
yousef yahia <yoousefyahia@gmail.com>
2025-11-18 14:15:31 +0200

    fix token type and add genral auth seeting &fix login success

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../Components/Header/Header";
 import Footer from "../Components/Footer/Footer";
-import "./MainLayout.css";
+import "./mainLayout.css";
 
 const MainLayout = ({ children }) => {
   return (
