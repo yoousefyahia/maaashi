c2093de8f929dbfa9f9e48ef975cdf8ca282372e
yousef yahia <yoousefyahia@gmail.com>
2025-12-06 21:20:00 +0200

    add ads im0g

--- src/Components/CategoriesSection/CategoriesSection.jsx
+++ src/Components/CategoriesSection/CategoriesSection.jsx
@@ -24,7 +24,7 @@ const CategoriesSection = () => {
     fetchCategories();
   }, []);
 
-  if (loading) return <p>جاري تحميل الأقسام...</p>;
+  if (loading) return <p className="loadCat">جاري تحميل الأقسام...</p>;
   if (error) return <p>{error}</p>;
 
   return (
